71672d5034c32b255e143ed4c5f14a4a02d13e41
Smytle <Yurianhooiveld@gmail.com>
2024-11-04 13:35:25 +0100

    dwada
    fweq

---
+++ my-app/src/app/components/Cookie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Cookie(props) {
+    return (
+        <button>
+            <Image
+            src="/DAANKOEKJE.png"
+            alt="COOKKIIEEEEE"
+            width={180}
+            height={38}
+            />
+        </button>
+    );
+}
+
+export default Cookie;
